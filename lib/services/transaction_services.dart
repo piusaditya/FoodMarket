1ef632fcc308f64958b8657ad72fd52e54891aee
Pius Aditya <piusadityanugraha@gmail.com>
2021-06-10 22:27:16 +0700

    transaction_services and order history page

--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -1,10 +1,30 @@
 part of 'services.dart';
 
 class TransactionServices {
-  static Future<ApiReturnValue<List<Transaction>>> getTransactions() async {
-    await Future.delayed(Duration(seconds: 3));
+  static Future<ApiReturnValue<List<Transaction>>> getTransactions(
+      {http.Client client}) async {
+    client ??= http.Client();
 
-    return ApiReturnValue(value: mockTransactions);
+    String url = baseURL + 'transaction';
+
+    var response = await client.get(Uri.parse(url), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${User.token}'
+    });
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Please try again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    List<Transaction> transactions = (data['data']['data'] as Iterable)
+        .map((e) => Transaction.fromJson(e))
+        .toList();
+
+    return ApiReturnValue(value: transactions);
+    // await Future.delayed(Duration(seconds: 3));
+    // return ApiReturnValue(value: mockTransactions);
   }
 
   static Future<ApiReturnValue<Transaction>> submitTransaction(
