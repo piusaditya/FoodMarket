9dd2a0abefc44bc9d087cd215c16bf0805c4c7d4
Pius Aditya <piusadityanugraha@gmail.com>
2021-06-10 23:01:10 +0700

    fix transaction page, add payment method page

--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -28,12 +28,38 @@ class TransactionServices {
   }
 
   static Future<ApiReturnValue<Transaction>> submitTransaction(
-      Transaction transaction) async {
-    await Future.delayed(Duration(seconds: 2));
+      Transaction transaction,
+      {http.Client client}) async {
+    client ??= http.Client();
 
-    //return ApiReturnValue(message: 'Transaksi Gagal');
-    return ApiReturnValue(
-        value:
-            transaction.copyWith(id: 123, status: TransactionStatus.pending));
+    String url = baseURL + 'checkout';
+    var response = await client.post(Uri.parse(url),
+        headers: {
+          "Content-Type": "application/json",
+          "Authentication": "Bearer ${User.token}"
+        },
+        body: jsonEncode(<String, dynamic>{
+          'food_id': transaction.food.id,
+          'user_id': transaction.user.id,
+          "quantity": transaction.quantity,
+          "total": transaction.total,
+          "status": "PENDING"
+        }));
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Please try again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    Transaction value = Transaction.fromJson(data['data']);
+    return ApiReturnValue(value: value);
+
+    // await Future.delayed(Duration(seconds: 2));
+
+    // //return ApiReturnValue(message: 'Transaksi Gagal');
+    // return ApiReturnValue(
+    //     value:
+    //         transaction.copyWith(id: 123, status: TransactionStatus.pending));
   }
 }
